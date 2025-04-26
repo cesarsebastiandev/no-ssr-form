ad03a33462b5dd99a3052065f5691531c104652d
cesarsebastiandev <cesarsebastian.dev@gmail.com>
2025-04-26 15:39:51 -0600

    Feat new comps for my app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'modal',
+    component: ModalComponent,
+  },
+  {
+    path: 'buttons',
+    component: ButtonsComponent,
+  },
+
+  {
+    path: '**',
+    component: ErrorComponent,
+  },
+];
